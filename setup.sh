766eb47ab9d8ed711da8e6e0d094c181a1da46bb
Luca Reggiannini <luca.reggiannini@gmail.com>
2022-04-26 03:03:07 +0200

    Fixed pip packages and added some instructions for python-magic

--- setup.sh
+++ setup.sh
@@ -4,4 +4,10 @@ mkdir "./samples"
 rm -rf "./dependencies/oledump.py" "./dependencies/pdf-parser.py"
 wget -c https://raw.githubusercontent.com/DidierStevens/DidierStevensSuite/master/oledump.py --directory-prefix ./dependencies
 wget -c https://raw.githubusercontent.com/DidierStevens/DidierStevensSuite/master/pdf-parser.py --directory-prefix ./dependencies
-pip install olefile tempfile
+pip install olefile tempfile python-magic
+
+echo "To make python-magic work please run the following commands (based on your platform):"
+echo "Debian/Ubuntu : sudo apt-get install libmagic1"
+echo "Windows       : pip install python-magic-bin"
+echo "OSX Homebrew  : brew install libmagic"
+echo "OSX macports  : port install file"
