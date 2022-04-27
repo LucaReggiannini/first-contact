85362fda0f0d052ffbde446ca62a2e386bfe3edc
Luca Reggiannini <luca.reggiannini@gmail.com>
2022-04-27 17:47:56 +0200

    Added rtfdump.py dependency

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 mkdir "./dependencies"
 mkdir "./samples"
-rm -rf "./dependencies/oledump.py" "./dependencies/pdf-parser.py"
+rm -rf "./dependencies/oledump.py" "./dependencies/pdf-parser.py" "./dependencies/rtfdump.py"
 wget -c https://raw.githubusercontent.com/DidierStevens/DidierStevensSuite/master/oledump.py --directory-prefix ./dependencies
 wget -c https://raw.githubusercontent.com/DidierStevens/DidierStevensSuite/master/pdf-parser.py --directory-prefix ./dependencies
+wget -c https://raw.githubusercontent.com/DidierStevens/DidierStevensSuite/master/rtfdump.py --directory-prefix ./dependencies
 pip install olefile tempfile python-magic
 
 echo "To make python-magic work please run the following commands (based on your platform):"
